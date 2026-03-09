5e1da86a8d1fe41b2284c817ad309232c46672c9
sanjana-dey-03 <sanjana@capitalnumbers.net>
2026-03-09 20:39:50 +0530

    Prepare server for deployment

--- client/server/server.js
+++ client/server/server.js
@@ -31,4 +31,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log(err));
 
 // start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
